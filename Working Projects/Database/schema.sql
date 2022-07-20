657c11e843b0d75e7a015f842caca4082fa3dd9c
scottish1176 <104025494+scottish1176@users.noreply.github.com>
2022-07-19 22:32:38 -0400

    work on SQL Drops

--- Working Projects/Database/schema.sql
+++ Working Projects/Database/schema.sql
@@ -1,8 +1,16 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS seq_recipe_id;
+
 DROP TABLE IF EXISTS recipe;
+DROP SEQUENCE IF EXISTS seq_recipe_id;
+DROP TABLE IF EXISTS ingredient;
+DROP SEQUENCE IF EXISTS seq_ingredient_id;
+DROP TABLE IF EXISTS measurement_units;
+DROP SEQUENCE IF EXISTS seq_measurement_units_id;
+DROP TABLE IF EXISTS measurement_qty;
+DROP SEQUENCE IF EXISTS seq_measurement_qty_id;
+DROP TABLE IF EXISTS recipe_ingredients;
+--DROP TABLE IF EXISTS users;
 
 
 CREATE SEQUENCE seq_recipe_id
@@ -19,53 +27,53 @@ CREATE TABLE recipe (
   notes_from_author varchar,
   original_source varchar,
   CONSTRAINT PK_recipe_id PRIMARY KEY (recipe_id)
-)
+);
 
 INSERT INTO recipe 
 (title, description, directions, notes_from_author, original_source)
-VALUES ('Taco Pie', "Annie's favorite recipie", 
-"1. Combine all dry crust ingredents in a bowl. 
+VALUES ('Taco Pie', 'Annie favorite', 
+'1. Combine all dry crust ingredents in a bowl. 
 2.Whisk the egg then combine with dry ingredients.
 3. knead the mixture with your hands and form the dough into a ball. Press the dough into pie plate as evenly as possible, Bake at 350 for 10-15 minutes (until slightly browned). Set aside.
 4. Brown the ground beef and then add the spice mixture, water, and salsa (if desired). cook for about 5 more minutes
 5. Pour into the prepared crust and bake at 350° far about 15 minutes.
-Serve with guacamole and pico!",
+Serve with guacamole and pico!',
 'Cayenne Pepper & Salsa are optional, and this is the first recipe ever put into this database :)',
 'Maxliving.com'); 
 
 
 CREATE SEQUENCE seq_ingredient_id
-INCRIMENT BY 1
+INCREMENT BY 1
 NO MAXVALUE
 START WITH 2000
 CACHE 1;
 
 CREATE TABLE ingredient (
   ingredient_id int DEFAULT nextval('seq_ingredient_id') NOT NULL,
-  ingredient_name varchar
-  CONSTRAINT PK_ingredient_id PRIMARY KEY (ingredient_id)
-)
+  ingredient_name varchar,
+  CONSTRAINT PK_ingredient PRIMARY KEY (ingredient_id)
+);
 
 INSERT INTO ingredient 
 (ingredient_id, ingredient_name)
 VALUES (2000, 'Almund Flour'),
-VALUES (2001, 'Sea Salt'),
-VALUES (2002, 'Chipotle Chili Powder'), 
-VALUES (2003, 'Baking Powder'),
-VALUES (2004, 'Egg'),
-VALUES (2005, 'Grass-fed Ground Beef'),
-VALUES (2006, 'Chili Powder'),
-VALUES (2007, 'Cumin'),
-VALUES (2008, 'Onion Powder'),
-VALUES (2009, 'Garlic Powder'),
-VALUES (2010, 'Oregano'),
-VALUES (2011, 'Cayenne Pepper'),
-VALUES (2012, 'Water'),
-VALUES (2013, 'Salsa');
+(2001, 'Sea Salt'),
+(2002, 'Chipotle Chili Powder'), 
+(2003, 'Baking Powder'),
+(2004, 'Egg'),
+(2005, 'Grass-fed Ground Beef'),
+(2006, 'Chili Powder'),
+(2007, 'Cumin'),
+(2008, 'Onion Powder'),
+(2009, 'Garlic Powder'),
+(2010, 'Oregano'),
+(2011, 'Cayenne Pepper'),
+(2012, 'Water'),
+(2013, 'Salsa');
 
 
 CREATE SEQUENCE seq_measurement_units_id
-INCRIMENT BY 1
+INCREMENT BY 1
 NO MAXVALUE
 START WITH 3000
 CACHE 1;
@@ -73,12 +81,12 @@ CACHE 1;
 CREATE TABLE measurement_units (
   measurement_units_id int DEFAULT nextval('seq_measurement_units_id') NOT NULL,
   measurement_description varchar,
-  CONSTRAINT PK_measurement_units_id PRIMARY KEY (measurement_units)
+  CONSTRAINT PK_measurement_units PRIMARY KEY (measurement_units_id)
 );
 
 
 CREATE SEQUENCE seq_measurement_qty_id
-INCRIMENT BY 1
+INCREMENT BY 1
 NO MAXVALUE
 START WITH 4000
 CACHE 1;
@@ -86,15 +94,19 @@ CACHE 1;
 CREATE TABLE measurement_qty (
   measurement_qty_id int DEFAULT nextval('seq_measurement_qty_id') NOT NULL,
   qty_amount varchar,
-  CONSTRAINT PK_measurement_qty_id PRIMARY KEY (measurement_qty_id)
+  CONSTRAINT PK_measurement_qty PRIMARY KEY (measurement_qty_id)
 );
 
 
 CREATE TABLE recipe_ingredients (
-  CONSTRAINT FK_recipe_id FOREIGN KEY (recipe_id),
-  CONSTRAINT FK_ingredient_id FOREIGN KEY (ingredient_id),
-  CONSTRAINT FK_measurement_units_id FOREIGN KEY (measurement_units_id),
-  CONSTRAINT FK_measurement_qty_id FOREIGN KEY (measurement_qty_id)
+  recipe_id int NOT NULL,
+  ingredient_id int NOT NULL,
+  measurement_units_id int NOT NULL,
+  measurement_qty_id int NOT NULL,
+  CONSTRAINT FK_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
+  CONSTRAINT FK_ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id),
+  CONSTRAINT FK_measurement_units FOREIGN KEY (measurement_units_id) REFERENCES measurement_units(measurement_units_id),
+  CONSTRAINT FK_measurement_qty FOREIGN KEY (measurement_qty_id) REFERENCES measurement_qty(measurement_qty_id)
 );
 
 
